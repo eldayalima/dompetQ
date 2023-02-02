8a13000cfb20ac7f051cdc743f6bfd927b313d51
Elwin Johan Sibarani <elwin.sibarani@dayadimensi.co.id>
2023-02-02 14:51:28 +0700

    20230202-first commit and finish setup project

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useState } from 'react';
+import Welcome from './screens/Welcome';
+import Home from './screens/Home';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
+  const [isAuthUser] = useState(false);
+
+  function AuthenticatedStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={Home}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  function UnauthenticatedStack() {
+    return (
+      <View style={styles.container}>
+        <Welcome />
+      </View>
+    );
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style="light" />
+      <NavigationContainer>
+        {!isAuthUser && <UnauthenticatedStack />}
+        {isAuthUser && <AuthenticatedStack />}
+      </NavigationContainer>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#131F33',
     alignItems: 'center',
     justifyContent: 'center',
   },
---
+++ constants/Colors.js
@@ -0,0 +1,5 @@
+const Colors = {
+  primary: '#131F33',
+};
+
+export default Colors;
