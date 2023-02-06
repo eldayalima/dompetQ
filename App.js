fc54364f8a460580cb7082de33e8467c48ea414d
Elwin Johan Sibarani <elwin.sibarani@dayadimensi.co.id>
2023-02-06 16:44:45 +0700

    20230206-add buttom bar navigation

--- App.js
+++ App.js
@@ -1,18 +1,99 @@
 /* eslint-disable camelcase */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { useState } from 'react';
 import {
   useFonts, Poppins_400Regular, Poppins_500Medium, Poppins_600SemiBold,
 } from '@expo-google-fonts/poppins';
+import { Ionicons } from '@expo/vector-icons';
 import Welcome from './screens/Welcome';
 import Home from './screens/Home';
 import Colors from './constants/Colors';
+import Transactions from './screens/Transactions';
+import Settings from './screens/Settings';
+import Assets from './screens/Assets';
 
 const Stack = createNativeStackNavigator();
+const ButtomTabs = createBottomTabNavigator();
 
+function ButtomNavigations() {
+  return (
+    <ButtomTabs.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: Colors.accents, height: 100 },
+        headerTitleStyle: { fontSize: 25 },
+        tabBarStyle: { backgroundColor: Colors.primary2, height: Platform.select({ ios: '10%', android: '8%' }) },
+        tabBarShowLabel: false,
+        headerTintColor: Colors.white,
+        tabBarActiveTintColor: Colors.yellow,
+      }}
+    >
+      <ButtomTabs.Screen
+        name="Home"
+        component={Home}
+        options={{
+          title: 'Home',
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'home-sharp' : 'home-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <ButtomTabs.Screen
+        name="Transactions"
+        component={Transactions}
+        options={{
+          title: 'Transactions',
+          tabBarLabel: 'Transactions',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'newspaper-sharp' : 'newspaper-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <ButtomTabs.Screen
+        name="Assets"
+        component={Assets}
+        options={{
+          title: 'Assets',
+          tabBarLabel: 'Assets',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'pie-chart' : 'pie-chart-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <ButtomTabs.Screen
+        name="Settings"
+        component={Settings}
+        options={{
+          title: 'Settings',
+          tabBarLabel: 'Settings',
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name={focused ? 'settings' : 'settings-outline'}
+              size={size}
+              color={color}
+            />
+          ),
+        }}
+      />
+    </ButtomTabs.Navigator>
+  );
+}
 export default function App() {
   const [isAuthUser, setIsAuthUser] = useState(false);
 
@@ -31,6 +112,11 @@ export default function App() {
           contentStyle: { backgroundColor: Colors.primary },
         }}
       >
+        <Stack.Screen
+          name="MenuOverview"
+          component={ButtomNavigations}
+          options={{ headerShown: false }}
+        />
         <Stack.Screen
           name="Home"
           component={Home}
--- constants/Colors.js
+++ constants/Colors.js
@@ -1,10 +1,12 @@
 const Colors = {
   primary: '#131F33',
+  primary2: '#101C2F',
   accents: '#1D293C',
   green: '#28BD52',
   blue: '#286DBD',
   red: '#E94C4C',
   white: '#FFFFFF',
+  yellow: '#F7BC0C',
 };
 
 export default Colors;
